3fbfac32d7bb296db8fbf6b0940edaefc5161938
asus <asus@LAPTOP-IRH01I8Q>
2021-10-23 00:02:21 +0300

    Добавьте файлы проекта.

---
+++ VariantA/Date.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VariantA
+{
+    class Date
+    {
+        // Дни недели
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thusday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        // Свойства класса
+        public DateYear Year { get; }
+        public DateMonth Month { get; }
+        public DateDay Day { get; }
+        // Конструкторы класса
+        public Date() {}
+        public Date(int day, int month, int year)
+        {
+            Year = new DateYear(year);
+            Month = new DateMonth(month);
+            Day = new DateDay(day);
+        }
+        // Методы класса
+        public DayOfWeek GetDayOfWeek() =>  
+            new DateTime(DateYear.Year,DateMonth.Month,DateDay.Day).DayOfWeek;  // День недели по заданному числу 
+        public int DaysBetween() // Количетсво дней между текущей датой и заданным числом
+        {
+            DateTime a = new DateTime(DateYear.Year, DateMonth.Month, DateDay.Day);
+            DateTime now = DateTime.Now;
+            return Math.Abs((now - a).Days);
+        } 
+        // Встроенные классы
+        public class DateYear
+        {
+            public bool Leap;
+            public static int Year { get; set; }
+            public DateYear(int year)
+            {
+                if (year >= 1 && year <= 3000)
+                    Year = year;
+                else
+                    throw new ArgumentException();
+                if (year % 4 == 0)// Проверка на високосность года
+                    Leap = true;
+                else 
+                    Leap = false;
+            } 
+            public override string ToString() => Year.ToString();
+        }
+        public class DateMonth
+        {
+            public static int Month { get; set; }
+            public DateMonth(int month)
+            {
+                if (month >= 1 && month <= 12)
+                    Month = month;
+                else
+                    throw new ArgumentException();
+            }
+            public int getDays(int monthNumber, bool leapYear)
+            {
+                switch (monthNumber)
+                {
+                    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                        return 31;
+                    case 4: case 6: case 9: case 11:
+                        return 30;
+                    case 2:
+                        if (leapYear == true)
+                            return 29;
+                        else
+                            return 28;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            } // Получение количества дней в месяце
+            public override string ToString() => Month.ToString();
+        }
+        public class DateDay
+        {
+            public static int Day { get; set; }
+            public DateDay(int day)
+            {
+                if (day >= 1 && day <= 31)
+                    Day = day;
+                else
+                    throw new ArgumentException();
+            }
+            public override string ToString() => Day.ToString();
+           /* public override bool Equals(object obj)
+            {
+                if (obj == null)
+                    return false;
+                DateDay otherDay = (DateDay)obj;
+                return (Day == otherDay.Day);
+            }*/
+        }
+        public static DayOfWeek ValueOf(int index) => 
+            (DayOfWeek)((int)(DateTime.Now.DayOfWeek + index) % 7); // День недели через месяц
+        public override string ToString() => $"{Day}.{Month}.{Year}";
+    }
+}
---
+++ VariantA/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariantA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date SomeDate = new Date(10,10,2021);
+            var TheDay = SomeDate.GetDayOfWeek();
+            var DayInMonth = SomeDate.Month.getDays(Date.DateMonth.Month, SomeDate.Year.Leap);
+            var AfterMonth = Date.ValueOf(DayInMonth);
+            var Between = SomeDate.DaysBetween();
+            Console.WriteLine($"Введенная дата: {SomeDate}, день недели: {TheDay}" +
+                $", количетсво дней в месяце: {DayInMonth}, день недели через месяц: {AfterMonth}" +
+                $", количество дней между текущей датой и введенной: {Between}");
+        }
+    }
+}
+
